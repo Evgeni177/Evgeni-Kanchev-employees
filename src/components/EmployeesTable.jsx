a2f653a86c982c0a032cf3b5741ce0dbd53d34e2
ATGeorgieva <tbibank@Emmas-MacBook-Air.local>
2024-02-19 00:01:55 +0200

    add thead and tbody

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -2,20 +2,24 @@ export default function EmployeesTable({ pairs }) {
   const [emp1, emp2] = pairs.key.split('-')
   return (
     <table>
-      <tr>
-        <th>Employee ID #1</th>
-        <th>Employee ID #2</th>
-        <th>Project ID</th>
-        <th>Days worked</th>
-      </tr>
-      {pairs.values.map(({projectId, days}) =>(
+      <thead>
         <tr>
-          <td>{emp1}</td>
-          <td>{emp2}</td>
-          <td>{projectId}</td>
-          <td>{days}</td>
+          <th>Employee ID #1</th>
+          <th>Employee ID #2</th>
+          <th>Project ID</th>
+          <th>Days worked</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {pairs.values.map(({projectId, days}) =>(
+          <tr key={days}>
+            <td>{emp1}</td>
+            <td>{emp2}</td>
+            <td>{projectId}</td>
+            <td>{days}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
